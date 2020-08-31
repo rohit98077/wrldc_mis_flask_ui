7dfc513cd216f7dc82eefb484307b2e941597a16
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-08-31 21:47:45 +0530

    changes

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ This is the web server that acts as a service that creates outages raw data
 '''
 from src.appConfig import getConfig
 from flask import Flask, request, jsonify, render_template
+# from waitress import serve
 
 app = Flask(__name__)
 
@@ -19,3 +20,4 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(appConfig['flaskPort']), debug=True)
+    # serve(app, host='0.0.0.0', port=int(appConfig['flaskPort']), threads=1)
