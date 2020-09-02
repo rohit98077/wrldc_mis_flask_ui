24e2fdc7b4b7048209edbf4d9f2220b015852bd8
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-09-02 06:38:18 +0530

    changes

---
+++ server_service.py
@@ -0,0 +1,11 @@
+"""
+    Run flask app as a windows service
+    https://stackoverflow.com/questions/23550067/deploy-flask-app-as-windows-service
+
+    Run flask app pyinstaller exe file as a windows service
+    https://stackoverflow.com/questions/55677165/python-flask-as-windows-service
+
+    use nssm to run flask app as a windows service
+    https://www.techcoil.com/blog/how-to-use-nssm-to-run-a-python-3-application-as-a-windows-service-in-its-own-python-3-virtual-environment/
+    https://nssm.cc/usage
+"""
