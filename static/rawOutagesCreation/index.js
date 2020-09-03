85faa866eeb24e5cc748cd467f9ab25e267006ec
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-09-03 09:36:11 +0530

    response decoration done

--- static/rawOutagesCreation/index.js
+++ static/rawOutagesCreation/index.js
@@ -1,15 +1,34 @@
 const createRawOutages = function() {
     const startDate = document.getElementById('startDate').value;
     const endDate = document.getElementById('endDate').value;
-
+    const respSelector = "#respDiv";
+    $(respSelector).text('Please wait...');
+    $(respSelector).css('border-color', 'transparent');
     $.ajax({
         type: 'post',
         dataType: 'json',
         contentType: "application/json",
         data: JSON.stringify({ startDate: startDate, endDate: endDate }),
-        success: function(response) {
-            // $('#response').text('Response : ' + response);
-            console.log(response)
+        success: function(resp) {
+            var borderColor = 'red';
+            if (resp.hasOwnProperty('isSuccess') && resp.hasOwnProperty('message')) {
+                if (resp['isSuccess']) {
+                    borderColor = 'green';
+                } else {
+                    borderColor = 'red';
+                }
+                $(respSelector).text(resp['message']);
+            } else {
+                $(respSelector).text(JSON.stringify(resp));
+            }
+            $(respSelector).css('border-color', borderColor);
+            console.log(resp);
+        },
+        error: function(jqXHR, exception) {
+            $(respSelector).css('border-color', 'red');
+            $(respSelector).html(jqXHR.responseText);
+            console.log(jqXHR);
+            // console.log(exception);
         }
     });
 }
